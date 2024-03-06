db8cb3acdb6b5b500c78639fff0886eb5f0d2abe
Erin <50000677+sinclairems@users.noreply.github.com>
2024-03-06 16:11:23 -0600

    install nodemon--updated package.json, got server working in practice.js, deleted code from server.js and added in working code from practice.js

--- server.js
+++ server.js
@@ -10,40 +10,18 @@
 // In order to delete a note, you'll need to read all notes from the db.json file, remove the note 
 // with the given id property, and then rewrite the notes to the db.json file.
 
-// const express = require('express');
-// const notes = require('./db.json');
 
-// const PORT = 3001;
+// BASIC Server Syntax -- Tutorial: Express JS Crash Course by Traversy Media
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+// Init express
+const app = express();
 
-// // GET route to get all of the terms
-// app.get('/api/notes', (req, res) => res.json(notes));
+const PORT = process.env.PORT || 3001;
 
-// // GET route that returns any specific term
-// app.get('/api/notes/:note', (req, res) => {
-//   // Coerce the specific search term to lowercase
-//   const requestedTerm = req.params.term.toLowerCase();
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//   // Iterate through the terms name to check if it matches `req.params.term`
-//   for (let i = 0; i < notes.length; i++) {
-//     if (requestedTerm === notes[i].term.toLowerCase()) {
-//       return res.json(notes[i]);
-//     }
-//   }
-
-//   // Return a message if the term doesn't exist in our DB
-//   return res.json('No match found');
-// });
-
-// // Fallback route for when a user attempts to visit routes that don't exist
-// app.get('*', (req, res) =>
-//   res.send(
-//     `Make a GET request using Insomnia to <a href="http://localhost:${PORT}/api/terms">http://localhost:${PORT}/api/terms</a>`
-//   )
-// );
-
-// // Listen for connections
-// app.listen(PORT, () =>
-//   console.info(`Example app listening at http://localhost:${PORT}`)
-// );
+// Listen on a port
+app.listen(3001, () => console.log(`Server started on port ${PORT}`));
